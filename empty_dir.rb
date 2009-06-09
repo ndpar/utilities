d58ed9ad24271e695335737fbdb9a09759fb76d0
Andrey Paramonov <git@ndpar.com>
2009-06-09 19:21:42 -0400

    Created empty_dir.rb script

---
+++ empty_dir.rb
@@ -0,0 +1,11 @@
+#!/usr/local/bin/ruby -w
+
+class Dir
+  def self.empty? dir_name
+    entries(dir_name).size == 2
+  end
+end
+
+Dir.glob("#{Dir.pwd}/**/*").each do |file_name|
+  puts file_name if File.directory? file_name and Dir.empty? file_name
+end
